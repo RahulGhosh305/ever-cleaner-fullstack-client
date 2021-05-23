54387d7ef638d12d38295a630adbfc15cddb6430
Rahul Ghosh <rahulghosh096@gmail.com>
2021-05-24 05:20:08 +0600

    resolved Dashboard and project almost done

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# This is Ever Cleaner Project
+# This is "Ever Cleaner" a MERN Stack Project
 
 **This is assignment full stack project.**
 ### In this project user can Order any service. And Payment With Stripe. Admin can manage Data and User Information.
 
-[Live](https://ever-cleaner.web.app/) link
+### Features
+
+- Client can choose any services and get order services by payment with stripe.
+- Build sign-up with google authentication and client dashboard to see products
+management.
+- Implement separate admin and client dashboards to manage product
+information and client management.
+
+[Demo](https://ever-cleaner.web.app/)
 
--- src/components/DashBoard/Dashboard/DashBoard.jsx
+++ src/components/DashBoard/Dashboard/DashBoard.jsx
@@ -1,75 +1,134 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../../../App';
-import Navbar from '../../Home/Navbar/Navbar';
+import React, { useContext } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import Book from '../Book/Book.jsx';
-import BookingList from '../BookingList/BookingList.jsx';
-import Review from '../Review/Review.jsx';
-import OrderList from '../OrderList/OrderList.jsx';
+import Review from '../Review/Review.jsx'
 import AddService from '../AddService/AddService.jsx';
+import BookingList from '../BookingList/BookingList.jsx';
 import MakeAdmin from '../MakeAdmin/MakeAdmin.jsx';
 import ManageService from '../ManageServices/ManageService.jsx';
+import OrderList from '../OrderList/OrderList.jsx';
+import { UserContext } from "../../../App.js";
+import { useState } from "react";
+import { useEffect } from "react";
+import Navbar from "../../Home/Navbar/Navbar.jsx";
+import './SideNavbar.css'
+
+const routes = [
+    {
+        path: "/book",
+        exact: true,
+        main: () => <Book></Book>
+    },
+    {
+        path: "/bookingList",
+        main: () => <BookingList></BookingList>
+    },
+    {
+        path: "/review",
+        main: () => <Review></Review>
+    },
+    {
+        path: "/orderList",
+        main: () => <OrderList></OrderList>
+    },
+    {
+        path: "/addService",
+        main: () => <AddService></AddService>
+    },
+    {
+        path: "/makeAdmin",
+        main: () => <MakeAdmin></MakeAdmin>
+    },
+    {
+        path: "/manageService",
+        main: () => <ManageService></ManageService>
+    },
+];
 
 
-const DashBoard = () => {
+export default function DashBoard() {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext)
-
     const [checkAdmin, setCheckAdmin] = useState([])
-    console.log(checkAdmin)
 
-    useEffect( ()=>{
-        fetch(`https://fast-springs-42794.herokuapp.com/getAdmin?email=`+loggedInUser.email)
-        .then(res => res.json())
-        .then(emails => setCheckAdmin(emails))
-    },[loggedInUser.email])
-
-
-
+    useEffect(() => {
+        fetch(`https://fast-springs-42794.herokuapp.com/getAdmin?email=` + loggedInUser.email)
+            .then(res => res.json())
+            .then(emails => setCheckAdmin(emails))
+    }, [loggedInUser.email])
 
     return (
-        <div className='container'>
-            <Navbar></Navbar>
-            <div className='row'>
-                <div style={{backgroundColor: '#ddd', height: '100vh', borderRadius : '25px'}} className="col-lg-3 col-md-3 col-sm-3 col-12">
-
-
-                    {
-                        checkAdmin ? 
-                        <div>
-                            <button className="mt-5 btn btn-secondary w-100 rounded-pill">Book</button>
-                            <button className="mt-2 btn btn-secondary w-100 rounded-pill">Booking Lists</button>
-                            <button className="mt-2 btn btn-secondary w-100 rounded-pill">Review</button>
-
-                            <button className="mt-5 btn btn-secondary w-100 rounded-pill">Order List</button>
-                            <button className="mt-2 btn btn-secondary w-100 rounded-pill">Add Service</button>
-                            <button className="mt-2 btn btn-secondary w-100 rounded-pill">Make Admin</button>
-                            <button className="mt-2 btn btn-secondary w-100 rounded-pill">Manage Services</button>
-                        </div> 
-                        : 
-                        <div>
-                            <button  className="mt-5 btn btn-secondary w-100 rounded-pill">Book</button>
-                            <button className="mt-2 btn btn-secondary w-100 rounded-pill">Booking Lists</button>
-                            <button className="mt-2 btn btn-secondary w-100 rounded-pill">Review</button>
-                        </div>
-                    }
-
-                    
-                </div>
-                <div className="col-lg-9 col-md-9 col-sm-9 col-12 mt-2">
-                    
-                    <Book></Book>
-                    <BookingList></BookingList>
-                    <Review></Review>
-                    <OrderList></OrderList>
-                    <AddService></AddService>
-                    <MakeAdmin></MakeAdmin>
-                    <ManageService></ManageService>
-
-                </div>
+        <>
+            <div className='container'>
+                <Navbar></Navbar>
             </div>
-        </div>
+            <Router>
+                <div className='container'>
+                    <div className='row'>
+                        <div style={{ backgroundColor: '#ddd', height: '100vh', borderRadius: '25px', }} className="col-lg-3 col-md-3 col-sm-3 col-12">
+                            { checkAdmin ?
+                                <ul className='list-group'>
+                                    <li className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/book" >Book</Link>
+                                    </li>
+                                    <li className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/bookingList">Booking List</Link>
+                                    </li>
+                                    <li className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/review">Review</Link>
+                                    </li>
+                                    <li className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/orderList">Order List</Link>
+                                    </li>
+                                    <li className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/addService">Add Service</Link>
+                                    </li>
+                                    <li className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/makeAdmin">Make Admin</Link>
+                                    </li>
+                                    <li className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/manageService">Manage Services</Link>
+                                    </li>
+                                </ul>
+                                
+                                :
+                                
+                                <ul className='list-group'>
+                                    <li className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/book">Book</Link>
+                                    </li>
+                                    <li  className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/bookingList">Booking List</Link>
+                                    </li>
+                                    <li  className='custom-link'>
+                                        <Link className='sideNavbarLink' to="/review">Review</Link>
+                                    </li>
+                                </ul>
+                                    
+                            }
+                        </div>
+                        <div className="col-lg-9 col-md-9 col-sm-9 col-12 mt-2"> 
+                            <h4 className='text-center'>Hello! Your Dashboard Here...</h4>
+                            <Switch>
+                                {routes.map((route, index) => (
+                                    <Route
+                                        key={index}
+                                        path={route.path}
+                                        exact={route.exact}
+                                        children={<route.main />}
+                                    />
+                                ))}
+                            </Switch>                           
+                        </div>
+                    </div>
+                </div>
+            </Router>
+        </>
     );
-};
-
-export default DashBoard;
+}
 
 
